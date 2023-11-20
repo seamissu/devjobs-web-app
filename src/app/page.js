1c36a8a377b793e4e29bb9d4d25ec5811f141b6a
Jingning <seamissu@gmail.com>
2023-11-20 23:58:44 +1100

    create HTML backbone

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,15 @@
+import React from 'react';
+
 import Image from 'next/image';
 import styles from './page.module.scss';
 
 export default function Home() {
-  return <main className={styles.main}></main>;
+  return (
+    <div className={styles.wrapper}>
+      <header className={styles.header}></header>
+      <section></section>
+      <main></main>
+      <footer></footer>
+    </div>
+  );
 }
