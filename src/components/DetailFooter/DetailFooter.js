310d8868bde7c11323ca86df9ec24ebe657a11af
Jingning <seamissu@gmail.com>
2023-11-22 20:05:55 +1100

    finish draft of Detail page Footer area

--- src/components/DetailFooter/DetailFooter.js
+++ src/components/DetailFooter/DetailFooter.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import styles from './DetailFooter.module.scss';
 
+import Button from '../Button';
+
 function DetailFooter() {
-  return <footer className={styles.wrapper}></footer>;
+  return (
+    <footer className={styles.wrapper}>
+      <div className={styles.innerwrapper}>
+        <div>
+          <h3>Senior Software Engineer</h3>
+          <p>So Digital Inc.</p>
+        </div>
+        <Button width={141} height={48}>
+          Apply Now
+        </Button>
+      </div>
+    </footer>
+  );
 }
 
 export default DetailFooter;
